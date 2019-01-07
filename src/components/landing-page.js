74a2ed8d455f04db60f18ee40dee5ecf9348fa97
Joshua Adams <joshadams554@gmail.com>
2019-01-07 12:05:51 -0500

    woirking on splash

--- src/components/landing-page.js
+++ src/components/landing-page.js
@@ -1,21 +1,38 @@
 import React from 'react';
-import {connect} from 'react-redux';
-import {Link, Redirect} from 'react-router-dom';
+import { connect } from 'react-redux';
+import { Link, Redirect } from 'react-router-dom';
 
 import LoginForm from './login-form';
+import './splash.css';
 
 export function LandingPage(props) {
     // If we are logged in redirect straight to the user's dashboard
     if (props.loggedIn) {
         return <Redirect to="/dashboard" />;
     }
+    const aboutBox = (<div className="about"><h1>Learn Esperanto:</h1>
+        <h2>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis
+            praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias
+            excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui
+            officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum
+            quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta
+             nobis est eligendi optio cumque nihil impedit quo minus id quod maxime
+             placeat facere possimus, omnis voluptas assumenda est, omnis dolor
+             repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum
+             necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae
+             non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut
+             reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus
+             asperiores repellat."
 
+</h2>
+    </div>)
+    const login = (<LoginForm />);
+    const register = (<Link to="/register">Register</Link>);
+    const wrapped = (<div className="wrapper">{aboutBox}{login}{register}</div>);
     return (
-        <div className="home">
-            <h2>Welcome to Foo App</h2>
-            <LoginForm />
-            <Link to="/register">Register</Link>
-        </div>
+        <main className="splash-page">
+            {wrapped}
+        </main>
     );
 }
 
