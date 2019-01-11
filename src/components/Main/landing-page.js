406951e7d096196a9e7b1914e8e35cfe796c4260
Josh <joshadams554@gmail.com>
2019-01-11 12:32:27 -0500

    styled landing page

--- src/components/Main/landing-page.js
+++ src/components/Main/landing-page.js
@@ -7,36 +7,35 @@ import './main.css';
 
 export function LandingPage(props) {
     // If we are logged in redirect straight to the user's dashboard
+    const greetingH1 = (<h1>Learn Esperanto</h1>);
+    const greetingH2 = (<h2>Esperanto is a constructed auxiliary language. Its creator was
+        Ludovic Lazarus Zamenhof, a Polish eye doctor. He created the
+        language to make international communication easier. His goal was
+        to design Esperanto in such a way that people can learn it much
+        more easily than any other national language.
+    </h2>);
+    const greetingP = (< p > With "Lesperanto" you can learn to speak the international language
+        Esperanto in a step by step spaced repition
+        pattern!
+        </p >);
+    const regButton = (<button>Register</button>);
+    const login = (<LoginForm />);
+    const greeting = (<div className="greeting">{greetingH1}<hr></hr> {greetingH2} {greetingP}</div>);
+    const register = (<div className="register"><Link to="/register">{regButton}</Link></div>);
+    const greetBox = (<div className="welcome">{greeting}{login}{register}</div>);
+
     if (props.loggedIn) {
         return <Redirect to="/dashboard" />;
     }
-    const aboutBox = (<div className="about"><h1>Learn Esperanto</h1>
-        <h2>Esperanto is a constructed auxiliary language. Its creator was
-            Ludovic Lazarus Zamenhof, a Polish eye doctor. He created the
-            language to make international communication easier. His goal was
-            to design Esperanto in such a way that people can learn it much
-            more easily than any other national language.
-        </h2>
-        <p>With "Lesperanto" you can learn to speak the international language 
-            Esperanto in a step by step spaced repition
-            pattern!
-        </p>
-        <p>Learn to communicate globally!</p>
-
-    </div>)
-    const login = (<LoginForm />);
-    const regButton = (<button>Register</button>);
-    const register = (<div className="register"><Link to="/register">{regButton}</Link></div>);
-    const wrapped = (<div className="wrapper">{aboutBox}{login}{register}</div>);
-            return (
+    return (
         <main className="splash-page">
-                {wrapped}
-            </main>
-            );
-        }
-        
+            {greetBox}
+        </main>
+    );
+}
+
 const mapStateToProps = state => ({
-                loggedIn: state.auth.currentUser !== null
-        });
-        
-        export default connect(mapStateToProps)(LandingPage);
+    loggedIn: state.auth.currentUser !== null
+});
+
+export default connect(mapStateToProps)(LandingPage);
