afc585bf5610b1172e933574e0a32f1099b69809
Josh <joshadams554@gmail.com>
2019-02-05 08:24:19 -0500

    changed logo to name of app

--- src/components/Navbar/header-bar.js
+++ src/components/Navbar/header-bar.js
@@ -2,47 +2,50 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { clearAuth } from '../../actions/auth';
 import { clearAuthToken } from '../../local-storage';
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 import './navbar.css';
 
 export class HeaderBar extends React.Component {
-    logOut() {
-        this.props.dispatch(clearAuth());
-        clearAuthToken();
-    }
+  logOut() {
+    this.props.dispatch(clearAuth());
+    clearAuthToken();
+  }
 
-
-
-    render() {
-        const logOutButton = (<button onClick={() => this.logOut()}>Log out</button>);
-        const logoLink = (<Link to='/' className="left" >Logo</Link>);
-        // Only render the log out button if we are logged in
-        const navBar = (<nav className="navbar">
-
-            <h1><ul>
-                <li className="logo">{logoLink}</li>
+  render() {
+    const logOutButton = <button onClick={() => this.logOut()}>Log out</button>;
+    const logoLink = (
+      <Link to="/" className="left">
+        Lesperanto
+      </Link>
+    );
+    // Only render the log out button if we are logged in
+    const navBar = (
+      <nav className="navbar">
+        <h1>
+          <ul>
+            <li className="logo">{logoLink}</li>
+          </ul>
+        </h1>
+      </nav>
+    );
+    if (this.props.loggedIn) {
+      const navBar = (
+        <nav className="navbar">
+          <h1>
+            <ul>
+              <li className="logo">logo</li>
+              <li>{logOutButton}</li>
             </ul>
-            </h1>
-        </nav>);
-        if (this.props.loggedIn) {
-            const navBar = (<nav className="navbar">
-
-                <h1><ul>
-                    <li className="logo">logo</li>
-                    <li>{logOutButton}</li>
-                </ul>
-                </h1>
-            </nav>);
-        }
-        return (
-            <main>{navBar}</main>
-
-        );
+          </h1>
+        </nav>
+      );
     }
+    return <main>{navBar}</main>;
+  }
 }
 
 const mapStateToProps = state => ({
-    loggedIn: state.auth.currentUser !== null
+  loggedIn: state.auth.currentUser !== null,
 });
 
 export default connect(mapStateToProps)(HeaderBar);
